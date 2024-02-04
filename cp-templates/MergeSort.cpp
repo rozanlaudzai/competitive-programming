a60c3427b457436eb51629aa39160af6d15c2a51
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-04 19:59:28 +0700

    Create MergeSort.cpp

---
+++ cp-templates/MergeSort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &left, vector<int> &right, vector<int> &arr) {
+
+  int i = 0, l = 0, r = 0;
+
+  while(l < left.size() && r < right.size()) {
+    if(left[l] > right[r]) {
+      arr[i++] = right[r++];
+    }
+    else {
+      arr[i++] = left[l++];
+    }
+  }
+
+  while(l < left.size()) {
+    arr[i++] = left[l++];
+  }
+  while(r < right.size()) {
+    arr[i++] = right[r++];
+  }
+
+}
+
+void mergeSort(vector<int> &arr) {
+  if(arr.size() <= 1) {
+    return;
+  }
+  vector<int> left, right;
+
+  int m = arr.size() / 2;
+
+  for(int i = 0; i < m; ++i) {
+    left.push_back(arr[i]);
+  }
+  for(int i = m; i < arr.size(); ++i) {
+    right.push_back(arr[i]);
+  }
+
+  mergeSort(left);
+  mergeSort(right);
+  merge(left, right, arr);
+
+}
