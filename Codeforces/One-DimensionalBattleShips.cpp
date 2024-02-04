a5676fff8bd16fc762ec30bbcfd179a56b15ccf8
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-04 17:06:08 +0700

    Create One-DimensionalBattleShips.cpp

---
+++ Codeforces/One-DimensionalBattleShips.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define bpc __builtin_popcountll
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cout << ">>> " << #x << " -> " << x << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < n; ++i)
+#define UFOR(i, a, n) for (int i = a; i <= n; ++i)
+#define RFOR(i, a, n) for (int i = a; i >= n; --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double PI = acos(-1);
+
+int n, k, a, m, arr[mxn2];
+
+bool inva(int id) {
+  
+  int vis[n + 1] = {};
+  UFOR (i, 1, id) {
+    vis[arr[i]] = 1;
+  }
+  UFOR (i, 2, n) {
+    vis[i] += vis[i - 1];
+  }
+
+  int bisa = 0;
+  UFOR (i, a, n) {
+    if (!(vis[i] - vis[i - a])) {
+      ++bisa;
+      i += a;
+    }
+  }
+  return bisa < k;
+
+}
+
+void solve() {
+  cin >> n >> k >> a >> m;
+
+  UFOR (i, 1, m) {
+    cin >> arr[i];
+  }
+
+  int l = 1, r = m, ans = -1;
+  while (l <= r) {
+    int mid = (l + r) / 2;
+
+    if (inva(mid)) {
+      ans = mid;
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  cout << ans << '\n';
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
