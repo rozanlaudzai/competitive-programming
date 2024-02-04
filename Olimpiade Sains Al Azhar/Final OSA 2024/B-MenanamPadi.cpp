c08b7e3b1a6b5bc5a91f91f60de84f3a9580ae8b
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-04 16:25:11 +0700

    Create B-MenanamPadi.cpp

---
+++ Olimpiade Sains Al Azhar/Final OSA 2024/B-MenanamPadi.cpp
@@ -0,0 +1,38 @@
+/*
+
+Final Olimpiade Al Azhar 2024
+B. Menanam Padi
+
+*/
+
+#include <stdio.h>
+
+const int mn = 1000001;
+int n, m, q, a, b, ans, lx, ly;
+bool x[mn], y[mn];
+
+int main() {
+
+  scanf("%d%d%d", &n, &m, &q);
+
+  while (q--) {
+    ans = 0;
+
+    scanf("%d%d", &a, &b);
+
+    if (!x[a]) {
+      x[a] = true;
+      ++lx;
+      ans += m - ly;
+    }
+
+    if (!y[b]) {
+      y[b] = true;
+      ++ly;
+      ans += n - lx;
+    }
+
+    printf("%d\n", ans);
+  }
+
+}
