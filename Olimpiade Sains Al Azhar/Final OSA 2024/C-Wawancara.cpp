64e0363d0ac68a7d11cf3eb4022df63c004e5dcd
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-04 16:28:20 +0700

    Create C-Wawancara.cpp

---
+++ Olimpiade Sains Al Azhar/Final OSA 2024/C-Wawancara.cpp
@@ -0,0 +1,47 @@
+/*
+
+Final Olimpiade Al Azhar 2024
+C. Wawancara
+
+*/
+
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+using std::sort;
+using std::queue;
+using std::max;
+
+int main() {
+
+  int n, k, d;
+  scanf("%d%d%d", &n, &k, &d);
+  int a[n];
+
+  for (int &i : a) {
+    scanf("%d", &i);
+  }
+
+  sort(a, a + n);
+
+  queue<int> q;
+
+  for (int &i : a) {
+    if (q.size() == k) {
+
+      q.push(max(q.front(), i) + d);
+      q.pop();
+
+    } else {
+
+      q.push(i + d);
+
+    }
+  }
+
+  while (q.size() > 1) {
+    q.pop();
+  }
+
+  printf("%d", q.front());
+}
