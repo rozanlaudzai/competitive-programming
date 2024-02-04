8e022bcf707300dd8ffe621b1a7401051b74fc89
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-04 16:36:51 +0700

    Create D-NasiGoreng.cpp

---
+++ Olimpiade Sains Al Azhar/Final OSA 2024/D-NasiGoreng.cpp
@@ -0,0 +1,63 @@
+/*
+
+Final Olimpiade Al Azhar 2024
+D. Nasi Goreng
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 100000;
+
+int n, m, dp[301][301][2];
+char ent, a[301][301];
+
+int dfs(int x, int y, int last) { // right: 0, down: 1
+  if (x == n && y == m) {
+    return 0;
+  }
+  if (dp[x][y][last] != -1) {
+    return dp[x][y][last];
+  }
+
+  int res = mx;
+
+  if (y < m && a[x][y + 1] == '.') {
+    if (last) {
+      res = min(res, dfs(x, y + 1, 0) + 1);
+    } else {
+      res = min(res, dfs(x, y + 1, 0));
+    }
+  }
+  if (x < n && a[x + 1][y] == '.') {
+    if (last) {
+      res = min(res, dfs(x + 1, y, 1));
+    } else {
+      res = min(res, dfs(x + 1, y, 1) + 1);
+    }
+  }
+
+  return dp[x][y][last] = res;
+}
+
+int main() {
+  memset(dp, -1, sizeof dp);
+
+  cin >> n >> m;
+  
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      cin >> a[i][j];
+    }
+  }
+  int ans = mx;
+  if (a[1][2] == '.') {
+    ans = dfs(1, 2, 0);
+  }
+  if (a[2][1] == '.') {
+    ans = min(ans, dfs(2, 1, 1));
+  }
+
+  printf("%d\n", ans);
+}
