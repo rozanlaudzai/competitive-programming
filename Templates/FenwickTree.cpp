f247b761c33c2832a09418432624b555cf5a03d6
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-08 08:04:03 +0700

    Update FenwickTree.cpp

--- Templates/FenwickTree.cpp
+++ Templates/FenwickTree.cpp
@@ -5,7 +5,7 @@ const int mxn = 2e5 + 5;
 int n, a[mxn];
 
 void upd(int idx, int val) {
-  while(idx <= n) {
+  while (idx <= n) {
     a[idx] += val;
     idx += idx & -idx;
   }
@@ -13,7 +13,7 @@ void upd(int idx, int val) {
 
 int sum(int idx) { // from index 1 until idx
   int res = 0;
-  while(idx > 0) {
+  while (idx > 0) {
     res += a[idx];
     idx -= idx & -idx;
   }
