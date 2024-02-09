1fcafe964f4995fcac6f617096232e31bd8b9d3e
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-10 00:26:31 +0700

    Update Main.cpp

--- Templates/Main.cpp
+++ Templates/Main.cpp
@@ -11,24 +11,29 @@ using namespace std;
 #define pob pop_back
 #define pf push_front
 #define pof pop_front
-#define bpc __builtin_popcountll
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
 #define g0 get<0>
 #define g1 get<1>
 #define g2 get<2>
 #define g3 get<3>
 #define g4 get<4>
+#define bpc __builtin_popcountll
 
-#define sz(x) (int)x.size()
-#define all(x) x.begin(), x.end()
-#define allr(x) x.rbegin(), x.rend()
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
 #define cmin(a, b) a = min(a, b)
 #define cmax(a, b) a = max(a, b)
 #define mems(arr, a) memset(arr, a, sizeof arr)
-#define db(x) cout << ">>> " << #x << " -> " << x << '\n'
+#define db(x) cout << ">>> " << #x << " -> " << (x) << '\n'
 
-#define FOR(i, a, n) for (int i = a; i < n; ++i)
-#define UFOR(i, a, n) for (int i = a; i <= n; ++i)
-#define RFOR(i, a, n) for (int i = a; i >= n; --i)
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
 #define each(i, arr) for (auto &i : arr)
 
 const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
