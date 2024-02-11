9e60c239ac0efa50033cce73a5b9ba8febbe122f
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-11 20:21:05 +0700

    Create F-S=1.cpp

---
+++ AtCoder/ABC 340/F-S=1.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+#define bpc __builtin_popcountll
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cout << ">>> " << #x << " -> " << (x) << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double PI = acos(-1);
+
+/*
+
+abs(xb - ya) = 2
+
+xb - ya = 2
+xb - ya = -2
+
+*/
+
+int gcd(int a, int b, int& x, int& y) {
+  if (b == 0) {
+    x = 1;
+    y = 0;
+    return a;
+  }
+  int x1, y1;
+  int d = gcd(b, a % b, x1, y1);
+  x = y1;
+  y = x1 - y1 * (a / b);
+  return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+  g = gcd(abs(a), abs(b), x0, y0);
+  if (c % g) {
+    return false;
+  }
+
+  x0 *= c / g;
+  y0 *= c / g;
+  if (a < 0) x0 = -x0;
+  if (b < 0) y0 = -y0;
+  return true;
+}
+
+void solve() {
+  int a, b, x, y, g;
+  cin >> a >> b;
+
+  if (find_any_solution(a, -b, 2, x, y, g)) {
+    cout << y << ' ' << x << '\n';
+  } else if (find_any_solution(a, -b, -2, x, y, g)) {
+    cout << y << ' ' << x << '\n';
+  } else {
+    cout << -1 << '\n';
+  }
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
