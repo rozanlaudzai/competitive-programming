73433c1ae37e26679ea0043f835913cceedef2c4
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-12 11:54:53 +0700

    Create 2A-TempatWisata.cpp

---
+++ OSN (Indonesia NOI)/OSN 2019/2A-TempatWisata.cpp
@@ -0,0 +1,32 @@
+/*
+
+OSN 2019
+2A. Tempat Wisata
+
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define all(x) x.begin(), x.end()
+using namespace std;
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  
+  int n, m;
+  cin >> n >> m;
+  
+  vector<int> a(n), b(n), c(n), d(n);
+  
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i] >> b[i];
+    c[i] = a[i];
+    d[i] = b[i];
+  }
+  sort(all(a));
+  sort(all(b));
+
+  for (int i = 0; i < n; ++i) {
+    cout << (upper_bound(all(a), d[i]) - a.begin()) - (lower_bound(all(b), c[i]) - b.begin()) - 1 << '\n';
+  }
+}
