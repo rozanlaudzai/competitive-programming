2fecda713230bee636568c90cff90e159250f80b
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-02-25 09:15:23 +0700

    Create C-PerangDigit.cpp

---
+++ TLX/TROC #33/C-PerangDigit.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (n % 10 ? "Neb\n" : "Rep\n");
+
+}
