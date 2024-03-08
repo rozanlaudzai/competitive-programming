9d0bf75f4202248a6397b19fe89943c1d147ecfd
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-03-08 20:55:59 +0700

    Create FlightDiscount.cpp

---
+++ CSES/Graph Algorithms/FlightDiscount.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+#define bpc __builtin_popcountll
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cout << ">>> " << #x << " -> " << (x) << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mxn3 = 3e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double PI = acos(-1);
+
+/*
+
+
+
+*/
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> iii;
+
+int n, m, a, b, c, d[mxn1][2];
+vector<pii> adj[mxn1];
+
+void solve() {
+  cin >> n >> m;
+  while (m--) {
+    cin >> a >> b >> c;
+    adj[a].eb(c, b);
+  }
+
+  mems(d, 0x2f);
+  d[1][0] = 0;
+
+  priority_queue<iii, vector<iii>, greater<iii>> pq;
+  pq.em(d[1][0], 1, 0);
+
+  while (!pq.empty()) {
+    a = g0(pq.top()), b = g1(pq.top()), c = g2(pq.top());
+    pq.pop();
+
+    if (a > d[b][c]) {
+      continue;
+    }
+
+    each (i, adj[b]) {
+
+      if (d[i.se][c] > d[b][c] + i.fi) {
+        pq.em(d[i.se][c] = d[b][c] + i.fi, i.se, c);
+      }
+
+      if (!c && d[i.se][1] > d[b][c] + i.fi / 2) {
+        pq.em(d[i.se][1] = d[b][c] + i.fi / 2, i.se, 1ll);
+      }
+      
+    }
+  }
+
+  cout << d[n][1] << '\n';
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
