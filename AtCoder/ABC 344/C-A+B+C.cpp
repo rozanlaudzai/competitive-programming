e5cc01119d47e423b49d8119c0c14b63db3d8be2
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-03-10 12:36:27 +0700

    Create C-A+B+C.cpp

---
+++ AtCoder/ABC 344/C-A+B+C.cpp
@@ -0,0 +1,88 @@
+// prefix all the possible sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+#define bpc __builtin_popcountll
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cout << ">>> " << #x << " -> " << (x) << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mxn3 = 3e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double PI = acos(-1);
+
+unordered_map<int, int> vis;
+int n, m, l, x, q, a[105], b[105], c[105];
+
+void solve() {
+  cin >> n;
+  UFOR (i, 1, n) {
+    cin >> a[i];
+  }
+  cin >> m;
+  UFOR (i, 1, m) {
+    cin >> b[i];
+  }
+  cin >> l;
+  UFOR (i, 1, l) {
+    cin >> c[i];
+  }
+
+  UFOR (i, 1, n) {
+    UFOR (j, 1, m) {
+      UFOR (k, 1, l) {
+        vis[a[i] + b[j] + c[k]] = true;
+      }
+    }
+  }
+
+  cin >> q;
+  while (q--) {
+    cin >> x;
+    cout << (vis[x] ? "Yes\n" : "No\n");
+  }
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
