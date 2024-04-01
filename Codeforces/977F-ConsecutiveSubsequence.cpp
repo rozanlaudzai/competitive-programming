3fa2d709360d1fba9fc816cd612fcbe352d38a6a
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-04-01 19:56:56 +0700

    Create 977F-ConsecutiveSubsequence.cpp

---
+++ Codeforces/977F-ConsecutiveSubsequence.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+#define bpc __builtin_popcountll
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cerr << ">>> " << #x << " -> " << (x) << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mxn3 = 3e5 + 10, mod = 1e9 + 7, inf = 1000000000000000001;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double PI = acos(-1);
+
+/*
+
+
+
+*/
+
+int n, a[mxn2];
+map<int, int> dp;
+
+void solve() {
+  cin >> n;
+  UFOR (i, 1, n) {
+    cin >> a[i];
+    dp[a[i]] = dp[a[i] - 1] + 1;
+  }
+  int mx = 0, val;
+  UFOR (i, 1, n) {
+    if (dp[a[i]] > mx) {
+      mx = dp[a[i]];
+      val = a[i];
+    }
+  }
+
+  vector<int> ans;
+  int id = n, babu = dp[val];
+  while (babu--) {
+    while (a[id] != val) {
+      --id;
+    }
+    ans.pb(id);
+    --val;
+  }
+  reverse(all(ans));
+  cout << sz(ans) << '\n';
+  each (i, ans) {
+    cout << i << ' ';
+  }
+  cout << '\n';
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
