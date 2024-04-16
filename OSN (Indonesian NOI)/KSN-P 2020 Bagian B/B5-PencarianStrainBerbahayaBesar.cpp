cb9153dea87d3439fd0f9eabd7e21858d73d5aa1
Rozan Laudzai <83082659+rozanlaudzai@users.noreply.github.com>
2024-04-16 22:39:39 +0700

    Create B5-PencarianStrainBerbahayaBesar.cpp

---
+++ OSN (Indonesian NOI)/KSN-P 2020 Bagian B/B5-PencarianStrainBerbahayaBesar.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ld long double
+#define fi first
+#define se second
+#define mp make_pair
+#define mt make_tuple
+#define ins insert
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define em emplace
+#define eb emplace_back
+#define ef emplace_front
+#define lb lower_bound
+#define ub upper_bound
+#define g0 get<0>
+#define g1 get<1>
+#define g2 get<2>
+#define g3 get<3>
+#define g4 get<4>
+#define bpc __builtin_popcountll
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define cmin(a, b) a = min(a, b)
+#define cmax(a, b) a = max(a, b)
+#define mems(arr, a) memset(arr, a, sizeof arr)
+#define db(x) cerr << ">>> " << #x << " -> " << (x) << '\n'
+
+#define FOR(i, a, n) for (int i = a; i < (n); ++i)
+#define UFOR(i, a, n) for (int i = a; i <= (n); ++i)
+#define RFOR(i, a, n) for (int i = a; i >= (n); --i)
+#define each(i, arr) for (auto &i : arr)
+
+const int mxn1 = 1e5 + 10, mxn2 = 2e5 + 10, mxn3 = 3e5 + 10, mod = 1e9 + 7;
+const int dx[] = {1, 0, -1, 0, 1, -1, 1, -1}, dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const ld PI = acos(-1);
+
+/*
+
+
+
+*/
+
+int n, s, p, mx, a[1000005];
+vector<int> adj[1000005];
+set<string> ch;
+map<string, vector<int>> st;
+
+void dfs(int x, int cnt) {
+  cnt += a[x];
+  cmax(mx, cnt);
+
+  each (i, adj[x]) {
+    dfs(i, cnt);
+  }
+}
+
+void solve() {
+  cin >> n >> s >> p;
+  UFOR (i, 1, n) {
+    cin >> a[i];
+  }
+
+  string oof;
+  int x, y;
+  UFOR (i, 1, p) {
+    cin >> oof >> y;
+    ch.ins(oof);
+    st[oof].pb(y);
+  }
+  UFOR (i, 1, n - p) {
+    cin >> x >> y;
+    adj[x].pb(y);
+  }
+
+  pair<int, string> ans;
+
+  each (c, ch) {
+    mx = 0;
+
+    each (oof, st[c]) {
+      dfs(oof, 0);
+    }
+
+    cmax(ans, mp(mx, c));
+  }
+
+  cout << ans.se << '\n' << ans.fi << '\n';
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  // int x = 1;
+  // int tc; cin >> tc; while (tc--)
+  // {
+  // cout << "Case #" << x++ << ": ";
+  solve();
+  // }
+
+}
